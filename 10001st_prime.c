c1b88718caede5bfd5236ce2bdef35a18901f785
Lina Zhao <lina.oahz@gmail.com>
2013-01-10 00:14:09 +0800

    update

--- 10001st_prime.c
+++ 10001st_prime.c
@@ -1,9 +1,4 @@
-/* Problem 7
-By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
-
-What is the 10 001st prime number?
-*/
-
+// Problem 7
 //written by lina <lina.oahz@gmail.com> on Wed Jan  9 23:17:33 SGT 2013
 ///////// compile////////////
 //gcc -Wall -O3 -o 10001st_prime 10001st_prime.c -lm
--- sum_square_differences.c
+++ sum_square_differences.c
@@ -1,12 +1,4 @@
-/* Problem 6
-The sum of the squares of the first ten natural numbers is,
-12 + 22 + ... + 102 = 385
-
-The square of the sum of the first ten natural numbers is,
-(1 + 2 + ... + 10)2 = 552 = 3025
-
-Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
-
+/*
 Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
 */
 // written by lina <lina.oahz@gmail.com> on Mon Jan  7 18:07:06 SGT 2013
