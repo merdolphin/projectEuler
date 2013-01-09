f6007422f05f720dc776bc869501c46217dca863
Lina Zhao <lina.oahz@gmail.com>
2013-01-09 23:19:03 +0800

    update

--- 10001st_prime.c
+++ 10001st_prime.c
@@ -4,29 +4,41 @@ By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that 
 What is the 10 001st prime number?
 */
 
-#include <stdio.h>
+//written by lina <lina.oahz@gmail.com> on Wed Jan  9 23:17:33 SGT 2013
+///////// compile////////////
+//gcc -Wall -O3 -o 10001st_prime 10001st_prime.c -lm
+/////////////////////////////
 
-int main(){
-   
-    int count = 1;
-    long i;
-    long n = 2;
+#include <stdio.h>
+#include <math.h>
+
+#define PRIME_MAX 6      
+
+int is_prime(int N){
+    int i;
     
-    while(1){
-        
-        for(i=2; i<n; i++){
-            if(n%i == 0){
-                n = n+1;
-                break;
-            }
+    if(N == 2)
+        return 1;
+
+    for(i=2; i<sqrt(N)+1; i++)
+        if(N%i == 0){
+            return 0;
         }
-        if(i == n){
-            count += 1;
-            printf("%ld is the %dth primer.\n", n, count);
-            }
-        if(count == 1001)
+     return 1;
+}
+
+int main(){   
+    
+    int counter = 0;
+    int j;
+
+    for(j=2;j<200000;j++){
+        if(is_prime(j))
+            counter++;
+        if(counter == 10001)
             break;
-            }
+    }
+    printf("%d\n", j);
 
     return 0;
 }
