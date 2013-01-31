ca5e196a70e548912a8101cf3c12804099fc8856
Lina Zhao <lina.oahz@gmail.com>
2013-01-31 16:45:18 +0800

    update

--- problem34.c
+++ problem34.c
@@ -1,36 +1,58 @@
 /* problem34.c */
 
 // written by lina <lina.oahz@gmail.com> 
-// start: Tue Jan 29 17:35:31 SGT 2013
+// start: Thu Jan 31 14:21:22 SGT 2013
 //   end:
-
+// 
 
 #include <stdio.h>
-#define M 1000000
+#include <stdlib.h>
 
-int factorial(int N){
 
+#define N 100000000
+
+long factorial(int M){
     int i;
-    int sum = 1;
+    long sum = 1;
 
-    for(i=1; i<=N; i++)
+    if(M == 1)
+        return M;
+
+    for(i=2; i<=M; i++)
         sum *= i;
 
-    return sum;
+    return sum;      
 }
 
 int main(){
-    
+
+    int i, j;
     int sum = 0;
 
-    //printf("%d\n",factorial(9)*8);
-    
-    int i, j;
+    char buf[16];
 
-
+    for(i=3; i<=N; i++){
         
-    
+        //buf = malloc(16* sizeof(char));
+        snprintf(buf, sizeof buf, "%d", i);
+        long s = 0;
+        
+        for(j=0; buf[j]!=0; j++){
+            s += factorial(buf[j]-'0');
+            
+        }
+        //printf("%d %ld\n",i, s);
 
+        //free(buf);
+           
+       if( s == i){
+            printf("%d\n",i); 
+            sum += i;
+       }
+    }
+
+    printf("%d\n",sum);
     return 0;
 
 }
+
