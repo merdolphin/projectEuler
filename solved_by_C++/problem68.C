0c6ce74e6932722f0aa6208a7531e18748afba2b
Lina Zhao <lina.oahz@gmail.com>
2013-02-18 01:00:01 +0800

    update

---
+++ solved_by_C++/problem68.C
@@ -0,0 +1,15 @@
+/* problem68.C */
+
+// written by lina <lina.oahz@gmail.com> 
+// start: Sun Feb 17 20:26:18 SGT 2013
+//   end:
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    return 0;
+}
+
+
