0b0069fdb6f30e67c70018a1fca8a77720001920
Lina Zhao <lina.oahz@gmail.com>
2013-02-25 15:22:39 +0800

    update

---
+++ problem32.C
@@ -0,0 +1,15 @@
+/* problem32.C */
+
+// written by lina <lina.oahz@gmail.com> 
+// start: Mon Feb 25 15:22:19 SGT 2013
+//   end:
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    return 0;
+}
+
+
