ad04c50a394be824aaf8f438b32ed16def6fe349
Lina Zhao <lina.oahz@gmail.com>
2013-02-26 18:20:14 +0800

    update

---
+++ isprime.h
@@ -0,0 +1,15 @@
+/** 
+*** Copyright (c) 2013 by
+*** lina <lina.oahz@gmail.com> 
+*** 
+**/
+
+#ifndef ISPRIME_H
+#define ISPRIME_H
+
+template <typename T> 
+int is_prime(T N);
+
+#include "isprime.C"
+
+#endif
---
+++ prime.h
@@ -0,0 +1,14 @@
+/** 
+*** Copyright (c) 2013 by
+*** lina <lina.oahz@gmail.com> 
+*** 
+**/
+
+#ifndef PRIME_H
+#define PRIME_H
+
+int is_prime(long N);
+
+#include "prime.C"
+
+#endif
