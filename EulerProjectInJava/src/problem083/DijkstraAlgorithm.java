4fa44e6f4dd3485b3ab24455903ec98c31867350
Lina Zhao <lina.oahz@gmail.com>
2013-08-19 23:54:13 +0800

    update

--- EulerProjectInJava/src/problem083/DijkstraAlgorithm.java
+++ EulerProjectInJava/src/problem083/DijkstraAlgorithm.java
@@ -1,7 +1,25 @@
 package problem083;
 
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import problem083.Edge;
+import problem083.Vertex;
+import problem083.Graph;
+
 public class DijkstraAlgorithm {
 
-
+	private final List<Vertex> nodes;
+	private final List<Edge> edges;
+	private Set<Vertex> settledNodes;
+	private Set<Vertex> unsettledNodes;
+	private Map<Vertex, Vertex> predecessors;
+	private Map<Vertex, Integer> distance;
+	
+	public DijkstraAlgorithm(Graph graph){
+		this.nodes = new ArrayList<Vertex>(graph.getVertexes());
+	}
 
 }
